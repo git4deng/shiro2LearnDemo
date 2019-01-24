0133173a0c7400f92417962a12d876bfdebf69f9
david <david@david.com>
2019-01-24 17:43:07 +0800

    项目初始化版本以及helloworld代码

---
+++ 1helloworld/com/david/shiro/helloworld/ShiroHelloworld.java
@@ -0,0 +1,79 @@
+package com.david.shiro.helloworld;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ShiroHelloworld {
+
+	private static final transient Logger log = LoggerFactory.getLogger(ShiroHelloworld.class);
+
+	public static void main(String[] args) {
+
+		
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		
+		SecurityManager securityManager =factory.getInstance();
+
+		SecurityUtils.setSecurityManager(securityManager);
+		Subject currentUser = SecurityUtils.getSubject();
+
+		Session session = currentUser.getSession();
+		
+		session.setAttribute("someKey", "aValue");
+		
+		String value = (String) session.getAttribute("someKey");
+		
+		if (value.equals("aValue")) {
+			log.info("Retrieved the correct value! [" + value + "]");
+		}
+		if (!currentUser.isAuthenticated()) {
+			UsernamePasswordToken token = new UsernamePasswordToken("lonestarr", "vespa");
+			token.setRememberMe(true);
+			try {
+				currentUser.login(token);
+			} catch (UnknownAccountException uae) {
+				log.info("There is no user with username of " + token.getPrincipal());
+			} catch (IncorrectCredentialsException ice) {
+				log.info("Password for account " + token.getPrincipal() + " was incorrect!");
+			} catch (LockedAccountException lae) {
+				log.info("The account for username " + token.getPrincipal() + " is locked.  "
+						+ "Please contact your administrator to unlock it.");
+			}
+			
+			catch (AuthenticationException ae) {
+				
+			}
+		}
+
+		log.info("User [" + currentUser.getPrincipal() + "] logged in successfully.");
+
+		
+		if (currentUser.hasRole("schwartz")) {
+			log.info("May the Schwartz be with you!");
+		} else {
+			log.info("Hello, mere mortal.");
+		}
+		if (currentUser.isPermitted("lightsaber:weild")) {
+			log.info("You may use a lightsaber ring.  Use it wisely.");
+		} else {
+			log.info("Sorry, lightsaber rings are for schwartz masters only.");
+		}
+		if (currentUser.isPermitted("winnebago:drive:eagle5")) {
+			log.info("You are permitted to 'drive' the winnebago with license plate (id) 'eagle5'.  "
+					+ "Here are the keys - have fun!");
+		} else {
+			log.info("Sorry, you aren't allowed to drive the 'eagle5' winnebago!");
+		}
+		currentUser.logout();
+		System.exit(0);
+	}
+
+}
